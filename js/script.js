bd9b9a2a3f87313e687b85ec9a12fc64a2283afd
donyamardani <zmardani0@gmail.com>
2025-09-25 21:29:41 +0330

    create function for chenge
    create function for chenge text in js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 "use strict"
+
+const creatSlug=str=>str.replaceAll(' ','-')
+const upperFirstWord=(str)=>{
+    const [first,...others]=str.split(' ')
+    return [first.toUpperCase(),...others].join(' ')
+}
+
+const selectEl=document.querySelector('#transform-func')
+const textAreaEl=document.querySelector('#str')
+const changeBtnEl=document.querySelector('#chenge-str')
+const resultEl=document.querySelector('#result')
+
+changeBtnEl.addEventListener('click',()=>{
+ const txt=textAreaEl.value
+ const funcType=selectEl.value
+ if(funcType=='slugify'){
+    textAreaEl.value=creatSlug(txt)
+ }else{
+    textAreaEl.value=upperFirstWord(txt)
+ }
+})
