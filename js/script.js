98f0855edb3ba6dd389dc03a27d4a37315d6c61c
donyamardani <zmardani0@gmail.com>
2025-09-29 21:54:02 +0330

    add new function
    add new function for upper letter

--- js/script.js
+++ js/script.js
@@ -1,10 +1,24 @@
 "use strict"
 
 const creatSlug=str=>str.replaceAll(' ','-')
-const upperFirstWord=(str)=>{
-    const [first,...others]=str.split(' ')
-    return [first.toUpperCase(),...others].join(' ')
+//upper first word
+//const upperFirstWord=(str)=>{
+//    const [first,...others]=str.split(' ')
+//    return [first.toUpperCase(),...others].join(' ')
+//}
+//upper first letter
+const upperFirstLetter=(str)=>{
+   const arrayStr=str.split(' ')
+   let newStr=[]
+   for(let word of arrayStr){
+      const letter=[...word]
+      letter[0]=letter[0].toUpperCase()
+      const newWord=letter.join('')
+      newStr.push(newWord)
+   }
+   return newStr.join(' ')
 }
+
 
 const selectEl=document.querySelector('#transform-func')
 const textAreaEl=document.querySelector('#str')
@@ -17,7 +31,7 @@ changeBtnEl.addEventListener('click',()=>{
  if(funcType=='slugify'){
     textAreaEl.value=creatSlug(txt)
  }else{
-    textAreaEl.value=upperFirstWord(txt)
+    textAreaEl.value=upperFirstLetter(txt)
  }
  
 })
